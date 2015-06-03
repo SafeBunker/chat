97429c94f95a252033c38f25f182edc68e98f8f9
Accew <Accew@AccewT002>
2015-06-04 01:15:59 +0300

    new commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link type='text/css' rel='stylesheet' href='css/style.css'>
+    <meta charset='UTF-8'>
+    <title>ChatWish</title>
+</head>
+
+<body>
+<div class='main'>
+    <div class='header'>
+        <span class='textHeader'>Dat ChatWish</span>
+    </div>
+    
+    <div class='bumper'></div>
+    <div class='footer'>
+        <span class='textFooter'>Poory data line about site.</span>
+        <span class='textFooterDate'>
+            <?php
+                echo date('d.m.Y');
+            ?>
+        </span>
+    </div>
+</div>
+</body>
+</html>
